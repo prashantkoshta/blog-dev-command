12d20a5f7ad31d7ceae0c3b4a973b469ce177e05
Prashant Koshta <prashantkoshta@gmail.com>
2016-10-22 19:45:20 -0500

    Fix folder path.

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
+var fs = require('fs');
+var path = require('path');
+var ejs = require('ejs');
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-
+app.set('views', __dirname + '/private');
+app.set('view engine', 'ejs'); // set up ejs for templating
+app.engine('.html', ejs.renderFile);
 app.use(express.static(__dirname + '/public'));
+
+app.get("/",function(req,res){
+	res.render('git/index.html');
+});
+app.get("/git",function(req,res){
+	res.render('git/index.html');
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));

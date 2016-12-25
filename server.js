87db4eb3d814a1854e24761c197abbcbed7531f8
Prashant Koshta <prashantkoshta@gmail.com>
2016-12-25 15:08:41 -0600

    Added site map template.

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ app.get("/git",function(req,res){
 app.get("/linux",function(req,res){
 	res.render('linux/index.html',{page_name:"linux"});
 });
+app.get("/sitemap.html",function(req,res){
+	res.render('sitemap.html',{page_name:"sitemaap"});
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));

fdbe84fa0f7eab24db13c52c1d739a39d3913e80
Prashant Koshta <prashantkoshta@gmail.com>
2017-01-10 00:36:15 -0600

    Added 404 Page.

--- src/app/shared/pagestatus404/pagestatus404.component.ts
+++ src/app/shared/pagestatus404/pagestatus404.component.ts
@@ -4,12 +4,12 @@ import {Title} from '@angular/platform-browser'
 @Component({
   selector: 'app-pagestatus404',
   templateUrl: './pagestatus404.component.html',
-  styleUrls: ['./pagestatus404.component.css']
+  styleUrls: ['./pagestatus404.component.scss']
 })
 export class Pagestatus404Component implements OnInit {
 
   constructor(private titleService: Title) { 
-    this.titleService.setTitle( "Contact Us" );
+    this.titleService.setTitle( "Error: Page Not Found" );
   }
 
   ngOnInit() {

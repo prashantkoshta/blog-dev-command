fca1eb58e42184d2647a78af30098f606380a016
Prashant Koshta <prashantkoshta@gmail.com>
2017-01-15 21:43:10 -0600

    Fix Path issue.

--- server.js
+++ server.js
@@ -28,6 +28,11 @@ app.get("/sitemap.html",function(req,res){
 	res.render('sitemap.html',{page_name:"sitemaap"});
 });
 */
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });

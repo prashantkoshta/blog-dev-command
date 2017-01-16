b0f0e6fd0a5f1840f739e0e410d215f1e006e4f1
Prashant Koshta <prashantkoshta@gmail.com>
2017-01-15 21:16:19 -0600

    Added build.

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.set('etag', 'strong');
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+/*
 app.set('views', __dirname + '/private');
 app.set('view engine', 'ejs'); // set up ejs for templating
 app.engine('.html', ejs.renderFile);
@@ -26,7 +27,7 @@ app.get("/linux",function(req,res){
 app.get("/sitemap.html",function(req,res){
 	res.render('sitemap.html',{page_name:"sitemaap"});
 });
-
+*/
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
